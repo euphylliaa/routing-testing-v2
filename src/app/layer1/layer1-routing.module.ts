ec817fd488db9db5bd20bbe940bdfc0fc0a175fc
Bryan Hsieh <Bryan.Hsieh1@T-Mobile.com>
2023-08-19 19:33:06 -0700

    layer1 children

--- src/app/layer1/layer1-routing.module.ts
+++ src/app/layer1/layer1-routing.module.ts
@@ -5,12 +5,14 @@ import { Layer1Component } from './layer1/layer1.component';
 const routes: Routes = [
   {
     path: '',
-    component: Layer1Component
-  },
-  {
-    path: 'layer2',
-    loadChildren: () => import('../layer2/layer2-routing.module').then(m => m.Layer2RoutingModule),
-    pathMatch: 'full'
+    component: Layer1Component,
+    children: [
+      {
+        path: 'layer2',
+        loadChildren: () => import('../layer2/layer2-routing.module').then(m => m.Layer2RoutingModule),
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
